5c437a83014eaa942c4f1383aa8c445424913150
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-05-20 19:53:03 -0700

    Port to Express

--- index.js
+++ index.js
@@ -1,1 +1,77 @@
-// Not sure what goes here yet
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
+const port = 8000;
+
+const movies = [
+    {
+        title: 'Casino Royale',
+        year: 2006,
+        rating: 'PG-13',
+        director: 'Martin Campbell',
+        stars: ['Daniel Craig', 'Eva Green', 'Judy Dench'],
+        series: '007',
+    },
+    {
+        title: 'Quantum Of Solace',
+        year: 2008,
+        rating: 'PG-13',
+        director: 'Marc Forster',
+        stars: ['Daniel Craig', 'Olga Kurylenko', 'Mathieu Amalric'],
+        series: '007',
+    },
+    {
+        title: 'Skyfall',
+        year: 2012,
+        rating: 'PG-13',
+        director: 'Sam Mendez',
+        stars: ['Daniel Craig', 'Javier Bardem', 'Naomie Harris'],
+        series: '007',
+    },
+];
+
+// File stream to append to log
+const logStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), { flags: 'a' });
+const logTemplate = ':date[iso] :method :url :status :res[content-length] - :response-time ms';
+
+function authorizeUser(req, res, next) {
+    // TODO: Impletement
+    // Denial of Authorization is a 404, not a redirect.
+    next();
+}
+
+// Run before Each request handler
+app.use(morgan(logTemplate, { stream: logStream }));
+
+app.use(authorizeUser);
+
+// Static files served from the /public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: `${__dirname}/public` });
+});
+
+app.get('/documentation', (req, res) => {
+    res.sendFile('documentation.html', { root: `${__dirname}/public` });
+});
+
+app.get('/movies', (req, res) => {
+    res.json(movies);
+});
+
+app.use((err, req, res, next) => {
+    const timestamp = new Date().toISOString();
+    console.log(err);
+    logStream.write(`${timestamp} Error: ${err.message}\n`);
+    logStream.write(`${timestamp} Error stack trace: ${err.stack}\n`);
+    logStream.write(`${timestamp} Error Request: ${JSON.stringify(req)}\n`);
+    res.status(500).send('server error occurred');
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
