90998c0fb8af71ba3222dc94c59511a6c974843a
bentleyjensen <bentleyjensen@users.noreply.github.com>
2022-06-25 10:26:11 -0700

    auth WIP

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # move-api
  a little movie api with details
+
+
+Make sure to update the JWTSecret in config/default.json! Or you'll get hacked :)
---
+++ passport.js
@@ -0,0 +1,44 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const Models = require('./models');
+const passportJWT = require('passport-jwt');
+const config = require('getconfig');
+
+const Users = Models.user;
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+
+passport.use(new LocalStrategy({
+    usernameField: 'Username',
+    passwordField: 'Password',
+}, (username, password, callback) => {
+    console.log(`Authenticating ${username} with password ${password}`);
+    Users.findOne({Username: username}, (err, user) => {
+        if (err) {
+            console.log(err);
+            return callback(err);
+        }
+
+        if (!user) {
+            console.log('User does not exist');
+            // TODO: Implement password checking
+            return callback(null, false, {message: 'Incorrect username password combination'});
+        }
+
+        console.log('Sign-in attempt complete');
+        return callback(null, user);
+    });
+}));
+
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.JWTSecret,
+}, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
+        .then((user) => {
+            return callback(null, user);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}));
